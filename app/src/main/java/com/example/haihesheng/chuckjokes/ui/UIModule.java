c21bc43c7d4d06d1f6e333d6ee5296db884e18a7
iamwindy <heshenghai94@gmail.com>
2018-04-03 02:42:35 +0200

    implemented MVP with dependency injection (dagger2)

---
+++ app/src/main/java/com/example/haihesheng/chuckjokes/ui/UIModule.java
@@ -0,0 +1,34 @@
+package com.example.haihesheng.chuckjokes.ui;
+
+import android.content.Context;
+
+import com.example.haihesheng.chuckjokes.ui.main.MainPresenter;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+
+/**
+ * Created by Hai on 2018-04-02.
+ */
+
+@Module
+public class UIModule {
+    private Context context;
+
+    public UIModule(Context context){
+        this.context = context;
+    }
+
+    @Provides
+    public Context provideContext() {
+        return context;
+    }
+
+    @Provides
+    @Singleton
+    public MainPresenter provideMainPresenter() {
+        return new MainPresenter();
+    }
+}
---
+++ app/src/main/java/com/example/haihesheng/chuckjokes/ui/main/MainPresenter.java
@@ -0,0 +1,19 @@
+package com.example.haihesheng.chuckjokes.ui.main;
+
+import com.example.haihesheng.chuckjokes.ui.Presenter;
+
+/**
+ * Created by Hai on 2018-04-02.
+ */
+
+public class MainPresenter extends Presenter<MainScreen> {
+    @Override
+    public void attachScreen(MainScreen screen){
+        super.attachScreen(screen);
+    }
+
+    @Override
+    public void detachScreen(){
+        super.detachScreen();
+    }
+}
